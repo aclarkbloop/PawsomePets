d6b094db9ceedb5457ad1330c5db346fa207e343
aclarkbloop <akclarknp@gmail.com>
2021-04-17 16:36:17 -0400

    firebase login working

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
-import fire from './fire';
-import Spotify from "./Spotify.js"
-
+// import Spotify from "./Components/Spotify.js";
+import Application from "./Components/Application.js";
+import UserProvider from "./providers/UserProvider.jsx";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { messages: [] }; // <- set up react state
-  }
-  componentWillMount(){
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
-    messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ messages: [message].concat(this.state.messages) });
-    })
-  }
-  addMessage(e){
-    e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref('messages').push( this.inputEl.value );
-    this.inputEl.value = ''; // <- clear the input
-  }
+  
+  
   render() {
     return (
-      <body>
-        <h1>Song-O-Meter</h1>
-        <Spotify />
-      </body>
+      <UserProvider>
+        <Application />
+      </UserProvider>
       
     );
   }
---
+++ src/providers/UserProvider.jsx
@@ -0,0 +1,26 @@
+import React, { createContext } from "react";
+import { Component } from 'react';
+import { auth, generateUserDocument } from "../firebase";
+
+export const UserContext = createContext({ user: null });
+class UserProvider extends Component {
+    state = {
+      user: null
+    };
+  
+    componentDidMount = async () => {
+        auth.onAuthStateChanged(async userAuth => {
+          const user = await generateUserDocument(userAuth);
+          this.setState({ user });
+        });
+    };
+
+    render() {
+      return (
+        <UserContext.Provider value={this.state.user}>
+          {this.props.children}
+        </UserContext.Provider>
+      );
+    }
+  }
+  export default UserProvider;
